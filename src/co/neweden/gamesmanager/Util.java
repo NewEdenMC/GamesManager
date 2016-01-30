6c1f56e7ee30a17697020fd0b59c2b62b1f608c1
aaron <aaron@neweden.co>
2016-01-30 00:16:51 +0000

    Utils.formatString now replaces formatting codes with unicode string instead of section character

--- src/co/neweden/gamesmanager/Util.java
+++ src/co/neweden/gamesmanager/Util.java
@@ -95,28 +95,28 @@ public final class Util {
 	}
 	
 	public static String formatString(String text) {
-		text = text.replaceAll("&0", "§0"); // Black
-		text = text.replaceAll("&1", "§1"); // Dark Blue
-		text = text.replaceAll("&2", "§2"); // Dark Green
-		text = text.replaceAll("&3", "§3"); // Dark Aqua
-		text = text.replaceAll("&4", "§4"); // Dark Red
-		text = text.replaceAll("&5", "§5"); // Dark Purple
-		text = text.replaceAll("&6", "§6"); // Gold
-		text = text.replaceAll("&7", "§7"); // Gray
-		text = text.replaceAll("&8", "§8"); // Dark Gray
-		text = text.replaceAll("&9", "§9"); // Blue
-		text = text.replaceAll("&a", "§a"); // Green
-		text = text.replaceAll("&b", "§b"); // Aqua
-		text = text.replaceAll("&c", "§c"); // Red
-		text = text.replaceAll("&d", "§d"); // Light Purple
-		text = text.replaceAll("&e", "§e"); // Yellow
-		text = text.replaceAll("&f", "§f"); // White
+		text = text.replaceAll("&0", "\u00A70"); // Black
+		text = text.replaceAll("&1", "\u00A71"); // Dark Blue
+		text = text.replaceAll("&2", "\u00A72"); // Dark Green
+		text = text.replaceAll("&3", "\u00A73"); // Dark Aqua
+		text = text.replaceAll("&4", "\u00A74"); // Dark Red
+		text = text.replaceAll("&5", "\u00A75"); // Dark Purple
+		text = text.replaceAll("&6", "\u00A76"); // Gold
+		text = text.replaceAll("&7", "\u00A77"); // Gray
+		text = text.replaceAll("&8", "\u00A78"); // Dark Gray
+		text = text.replaceAll("&9", "\u00A79"); // Blue
+		text = text.replaceAll("&a", "\u00A7a"); // Green
+		text = text.replaceAll("&b", "\u00A7b"); // Aqua
+		text = text.replaceAll("&c", "\u00A7c"); // Red
+		text = text.replaceAll("&d", "\u00A7d"); // Light Purple
+		text = text.replaceAll("&e", "\u00A7e"); // Yellow
+		text = text.replaceAll("&f", "\u00A7f"); // White
 		
-		text = text.replaceAll("&k", "§k"); // Obfuscated
-		text = text.replaceAll("&l", "§l"); // Bold
-		text = text.replaceAll("&m", "§m"); // Strikethrough
-		text = text.replaceAll("&o", "§o"); // Italic
-		text = text.replaceAll("&r", "§r"); // Reset
+		text = text.replaceAll("&k", "\u00A7k"); // Obfuscated
+		text = text.replaceAll("&l", "\u00A7l"); // Bold
+		text = text.replaceAll("&m", "\u00A7m"); // Strikethrough
+		text = text.replaceAll("&o", "\u00A7o"); // Italic
+		text = text.replaceAll("&r", "\u00A7r"); // Reset
 		
 		return text;
 	}
