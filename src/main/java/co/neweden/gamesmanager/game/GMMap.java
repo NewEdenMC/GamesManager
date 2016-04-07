13f77c874e1d44dde050fa2819385f5c6fcb11f2
aaron <aaron@neweden.co>
2016-04-07 20:53:03 +0100

    GMMap: new getConfig method returns the map config

--- src/main/java/co/neweden/gamesmanager/game/GMMap.java
+++ src/main/java/co/neweden/gamesmanager/game/GMMap.java
@@ -1,9 +1,11 @@
 package co.neweden.gamesmanager.game;
 
 import co.neweden.gamesmanager.Game;
+import co.neweden.gamesmanager.game.config.MultiConfig;
 import org.bukkit.Bukkit;
 import org.bukkit.WeatherType;
 import org.bukkit.World;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -11,18 +13,25 @@ import org.bukkit.event.world.WorldUnloadEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
+import java.io.FileNotFoundException;
+
 public class GMMap implements Listener {
 
     Game game;
     private World world;
     private String baseWorldName;
+    private YamlConfiguration config;
 
     public GMMap(Game game, World world, String baseWorldName) {
         this.game = game;
         this.world = world;
         this.baseWorldName = baseWorldName;
         Bukkit.getServer().getPluginManager().registerEvents(this, game.getPlugin());
-        
+        try {
+            config = game.getConfig().getIndividualConfig(MultiConfig.Type.MAP, baseWorldName + ".yml");
+        } catch (FileNotFoundException ex) {
+            game.getPlugin().getLogger().warning(String.format("[%s] Unable to fine map config for map %s, this could be a problem.", game.getName(), baseWorldName));
+        }
     }
 
     public World getWorld() {
@@ -31,6 +40,10 @@ public class GMMap implements Listener {
 
     public String getBaseWorldName() {
         return baseWorldName;
+    }
+
+    public YamlConfiguration getConfig() {
+        return config;
     }
 
     @EventHandler(priority = EventPriority.MONITOR)
